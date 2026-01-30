1561e8419a70bdcfb83dc967d5d80ecd847ed17c
KartikZCoding <ahir.kartik.385@gmail.com>
2026-01-30 10:25:55 +0530

    first basic project added with LINQ and filter example

---
+++ TCPData/Data.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPData
+{
+    public static class Data
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Bob",
+                    LastName = "Jones",
+                    AnnualSalary = 60000.3m,
+                    IsManager = true,
+                    DepartmentId = 1
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Sarah",
+                    LastName = "Jameson",
+                    AnnualSalary = 80000.1m,
+                    IsManager = true,
+                    DepartmentId = 2
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Douglas",
+                    LastName = "Roberts",
+                    AnnualSalary = 40000.2m,
+                    IsManager = false,
+                    DepartmentId = 2
+                },
+                new Employee
+                {
+                    Id = 4,
+                    FirstName = "Jane",
+                    LastName = "Stevens",
+                    AnnualSalary = 30000.2m,
+                    IsManager = false,
+                    DepartmentId = 3
+                }
+            };
+            return employees;
+        }
+
+        public static List<Department> GetDepartments()
+        {
+            List<Department> departments = new List<Department>
+            {
+                new Department
+                {
+                    Id = 1,
+                    ShortName = "HR",
+                    LongName = "Human Resources"
+                },
+                new Department
+                {
+                    Id = 2,
+                    ShortName = "FN",
+                    LongName = "Finance"
+                },
+                new Department
+                {
+                    Id = 3,
+                    ShortName = "TE",
+                    LongName = "Technology"
+                }
+            };
+            return departments;
+        }
+    }
+}
---
+++ ThePretendCompanyApplication/Program.cs
@@ -0,0 +1,70 @@
+﻿using TCPData;
+using TCPExtentions;
+
+namespace ThePretendCompanyApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = Data.GetEmployees();
+
+            //var filteredEmployees = employeeList.Filter(emp => emp.AnnualSalary > 50000);
+
+            //foreach (var employee in filteredEmployees)
+            //{
+
+            //    Console.WriteLine($"First Name: {employee.FirstName}");
+            //    Console.WriteLine($"Last Name: {employee.LastName}");
+            //    Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+            //    Console.WriteLine($"Manager: {(employee.IsManager ? "Yes" : "No")}");
+            //    Console.WriteLine();
+            //}
+
+            List<Department> departmentsList = Data.GetDepartments();
+
+            //var filteredDepartments = departmentsList.Where(dpt => dpt.Id > 1 );
+
+            //foreach (var department in filteredDepartments) {
+
+            //    Console.WriteLine($"Id: {department.Id}");
+            //    Console.WriteLine($"Short Name: {department.ShortName}");
+            //    Console.WriteLine($"Long Name: {department.LongName}");
+            //    Console.WriteLine();
+            //}
+
+            var resultList = from emp in employeeList
+                             join dept in departmentsList
+                             on emp.DepartmentId equals dept.Id
+                             select new
+                             {
+                                 FirstName = emp.FirstName,
+                                 LastName = emp.LastName,
+                                 AnnualSalary = emp.AnnualSalary,
+                                 Manager = (emp.IsManager ? "Yes" : "No"),
+                                 Department = dept.LongName
+                             };
+
+            foreach (var result in resultList)
+            {
+                Console.WriteLine($"First Name: {result.FirstName}");
+                Console.WriteLine($"Last Name: {result.LastName}");
+                Console.WriteLine($"Annual Salary: {result.AnnualSalary}");
+                Console.WriteLine($"Manager: {result.Manager}");
+                Console.WriteLine($"Department: {result.Department}");
+                Console.WriteLine();
+            }
+
+            var averageAnnualSalary = resultList.Average(a => a.AnnualSalary);
+            var highestAnnualSalary = resultList.Max(a => a.AnnualSalary);
+            var lowestAnnualSalary = resultList.Min(a => a.AnnualSalary);
+
+            Console.WriteLine($"Average Annual Salary: {averageAnnualSalary}");
+            Console.WriteLine($"Highest Annual Salary: {highestAnnualSalary}");
+            Console.WriteLine($"Lowest Annual Salary: {lowestAnnualSalary}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
